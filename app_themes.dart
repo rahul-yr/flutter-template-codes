ac69d07077b3afd18d5c079c0b68127fb194a508
rahul-yr <72339443+rahul-yr@users.noreply.github.com>
2021-06-05 11:36:47 +0530

    Create app_themes.dart

---
+++ app_themes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final ThemeData Light_Theme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    primaryColor: Colors.blueAccent,
+    accentColor: Colors.blue,
+    primaryIconTheme: IconThemeData(color: Colors.white),
+    accentIconTheme: IconThemeData(color: Colors.red),
+    fontFamily: 'Roboto',
+    textTheme: TextTheme(
+      headline1: TextStyle(fontSize: 38.0, color: Colors.purple),
+      headline6: TextStyle(fontSize: 36.0, color: Colors.purple),
+      bodyText2: TextStyle(fontSize: 14.0),
+    ),
+  );
+  static final ThemeData Dark_Theme = ThemeData(
+    scaffoldBackgroundColor: Colors.blue[900],
+    primaryColor: Colors.blue[800],
+    accentColor: Colors.black26,
+    primaryIconTheme: IconThemeData(color: Colors.white),
+    accentIconTheme: IconThemeData(color: Colors.red),
+    fontFamily: 'Roboto',
+    textTheme: TextTheme(
+      headline1: TextStyle(fontSize: 38.0, color: Colors.purple),
+      headline6: TextStyle(fontSize: 36.0, color: Colors.purple),
+      bodyText2: TextStyle(fontSize: 14.0),
+    ),
+  );
+  static final Map<String, ThemeData> Theme_Mode_Mapper = {"Light" : Light_Theme, "Dark" : Dark_Theme};
+  static const String Default_Theme = "Light";
+}
